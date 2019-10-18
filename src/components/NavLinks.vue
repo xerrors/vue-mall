a07af334004b65b05ea92a78ef7b5adf6737f976
root <root@Xerrors.localdomain>
2019-10-18 22:51:15 +0800

    new navbar

--- src/components/NavLinks.vue
+++ src/components/NavLinks.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="nav-links">
-    <router-link to='/test'>Test</router-link>
-    <router-link to='/test'>Test</router-link>
-    <router-link to='/test'>Test</router-link>
-    <router-link v-if="!online" to='/test'>登录/注册</router-link>
-    <span v-else class="user-avatar">
-      <img class="user-avatar__img" :src="user.avatar">
-    </span>
+    <router-link to='/test' class="nav-link">Test</router-link>
+    <router-link to='/test' class="nav-link">Test</router-link>
+    <router-link to='/test' class="nav-link">Test</router-link>
+    <div v-if="!logined" class="nav-right">
+      <el-button round plain @click="logined = true">登录</el-button>
+    </div>
+    <div v-else class="user-box nav-right">
+      <el-badge :is-dot="user.unread" class="item">
+        <el-link icon="el-icon-bell">消息</el-link>
+      </el-badge>
+      <el-dropdown type="primary" @command="dropdownCmd">
+        <el-avatar :src="user.avatar"></el-avatar>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>我的主页</el-dropdown-item>
+          <el-dropdown-item command="logout">退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
@@ -14,27 +25,39 @@
 export default {
   data () {
     return {
-      online: true
+      logined: false
     }
   },
   computed: {
     user () {
       var user = {
-        avatar: 'http://img3.imgtn.bdimg.com/it/u=4095182135,769981712&fm=26&gp=0.jpg'
+        avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+        unread: true
       }
       return user
+    }
+  },
+  methods: {
+    dropdownCmd () {
+      this.logined = false
     }
   }
 }
 </script>
 
 <style lang="stylus">
+nav-icon-size = 40px
 .nav-links
-  display flex
-  align-items center
-  justify-content space-between
+  .nav-link
+    display inline-block
   .user-avatar
     &__img
-      width 40px
-      height 40px
+      width nav-icon-size
+      height nav-icon-size
+  .user-box
+    display flex
+    align-items center
+    .el-button, .el-dropdown
+      margin 8px
+      height nav-icon-size
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,83 @@
 <template>
   <header class="navbar">
     <img class="logo" src="favicon.ico">
-    <nav-links/>
+    <div class="nav-links">
+      <router-link to='/test' class="nav-link">Test</router-link>
+      <router-link to='/test' class="nav-link">Test</router-link>
+      <router-link to='/test' class="nav-link">Test</router-link>
+    </div>
+    <div v-if="!logined" class="nav-right">
+      <el-button round plain @click="logined = true">登录</el-button>
+    </div>
+    <div v-else class="user-box nav-right">
+      <el-badge :is-dot="user.unread" class="item">
+        <el-link icon="el-icon-bell">消息</el-link>
+      </el-badge>
+      <el-dropdown type="primary" @command="dropdownCmd">
+        <el-avatar :src="user.avatar"></el-avatar>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>我的主页</el-dropdown-item>
+          <el-dropdown-item command="logout">退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+  </div>
   </header>
 </template>
 
 <script>
-import NavLinks from '@/components/NavLinks.vue'
-
 export default {
   nmae: 'nav-bar',
   components: {
-    NavLinks
+  },
+  data () {
+    return {
+      logined: false
+    }
+  },
+  computed: {
+    user () {
+      var user = {
+        avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+        unread: true
+      }
+      return user
+    }
+  },
+  methods: {
+    dropdownCmd () {
+      this.logined = false
+    }
   }
 }
 </script>
 
 <style lang="stylus">
-  .navbar
+navIconSize = 40px
+
+.navbar
+  display flex
+  align-items center
+
+  width 100%
+  height 50px
+
+  background-color navBgColor
+
+  .logo
+    width 40px
+    height 40px
+
+  .nav-links
+    .nav-link
+      display inline-block
+    .el-avatar
+      width navIconSize
+      height navIconSize
+
+  .user-box
     display flex
     align-items center
-    justify-content space-between
-
-    width 100%
-    height 50px
-
-    background-color navBgColor
-
-    .logo
-      width 40px
-      height 40px
+    .el-button, .el-dropdown
+      margin 8px
+      height navIconSize
 </style>
