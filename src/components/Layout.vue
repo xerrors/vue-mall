980e764614e7875178ba0d3da162c77cfae2d2a7
root <root@Xerrors.localdomain>
2019-10-21 11:10:44 +0800

    change the file system

---
+++ src/components/Layout.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="layout">
+    <header>
+      <navbar/>
+    </header>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+      <foot/>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Foot from '@/components/Foot.vue'
+
+export default {
+  name: 'layout',
+  components: {
+    Navbar,
+    Foot
+  }
+}
+</script>
+
+<style lang="stylus">
+::-webkit-scrollbar {
+  width: 0.3rem !important;
+}
+::-webkit-scrollbar-thumb {
+  background-color: #f2f2f2;
+  border-radius: 0.25rem;
+  transition: .3s ease;
+}
+::-webkit-scrollbar-thumb:hover {
+  background-color: #c2c2c2;
+  cursor: pointer;
+}
+
+::selection {
+  color: #fff;
+  background: #282c34;
+}
+
+.layout
+  min-height 100vh
+  display flex
+  flex-direction column
+  main
+    flex 1
+  footer
+    display flex
+    justify-content center
+    align-items center
+
+    height fheight
+    background-color footerBg
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,47 @@
 <template>
-  <div class="navbar">
-    <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Wikimedia-logo.png">
-    <div class="nav-links">
-      <router-link to='/test' class="nav-link" tag="div">公关部门</router-link>
-      <router-link to='/test' class="nav-link" tag="div">市场部门</router-link>
-      <router-link to='/test' class="nav-link" tag="div">人力部门</router-link>
-    </div>
-    <div v-if="!token" class="nav-right">
-      <el-button round plain @click="login">登录</el-button>
-    </div>
-    <div v-else class="user-box nav-right">
-      <el-badge :is-dot="user.unread" class="item">
-        <el-link icon="el-icon-message-solid">消息</el-link>
-      </el-badge>
-      <el-dropdown type="primary" @command="dropdownCmd">
-        <el-avatar :src="user.avatar"></el-avatar>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>我的主页</el-dropdown-item>
-          <el-dropdown-item command="logout">退出登录</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
+  <div>
+    <login :is-show='!token'/>
+    <div class="navbar">
+      <router-link to="/"><img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Wikimedia-logo.png"></router-link>
+      <div class="nav-links">
+        <router-link to='/test' class="nav-link" tag="div">公关部门</router-link>
+        <router-link to='/test' class="nav-link" tag="div">市场部门</router-link>
+        <router-link to='/test' class="nav-link" tag="div">人力部门</router-link>
+        <router-link to='/about' class="nav-link" tag="div">关于我们</router-link>
+      </div>
+      <div v-if="!token" class="nav-right">
+        <el-button round plain @click="login">登录</el-button>
+      </div>
+      <div v-else class="user-box nav-right">
+        <el-badge :is-dot="user.unread" class="item">
+          <!-- TODO 消息功能 -->
+          <el-link icon="el-icon-message-solid">消息</el-link>
+        </el-badge>
+        <el-dropdown type="primary" @command="dropdownCmd">
+          <el-avatar :src="avatar"></el-avatar>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>我的主页</el-dropdown-item>
+            <el-dropdown-item command="logout">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Login from '@/components/Login.vue'
+
 export default {
   nmae: 'nav-bar',
   components: {
+    Login
   },
   computed: {
     ...mapGetters(['token', 'avatar']),
     user () {
       var user = {
-        avatar: this.avatar,
         unread: true
       }
       return user
@@ -55,6 +62,10 @@ export default {
             console.log(err)
             this.loading = false
           })
+        this.$message({
+          message: '成功登出',
+          type: 'success'
+        })
       }
     }
   }
@@ -88,7 +99,6 @@ export default {
     .el-avatar
       width navIconSize
       height navIconSize
-      cursor pointer
   .nav-right
     margin-left auto
 
@@ -98,4 +108,5 @@ export default {
     .el-button, .el-dropdown
       margin 0 10px
       height navIconSize
+      cursor: pointer;
 </style>
