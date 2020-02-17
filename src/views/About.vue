b104a8eb0dc1df940146767ff26cea4259a0a2de
Xerrors <xerrors@163.com>
2020-02-17 22:47:33 +0800

    update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="about">
-    <img class="about__img" src='Logo_2.png'/>
-    <h2>项目介绍</h2>
-    <p>本项目为江南大学物联网工程学院的大创小组开发</p>
-
+    <img class="about__img" src="Logo_2.png" />
+    <h4>项目介绍</h4>
+    <p>本项目为江南大学物联网工程学院的大创小组开发，当前项目正处于测试阶段，数据部分还没有完善。不用想 GitHub 地址了，暂时没开源！</p>
+    <h4>权限信息</h4>
+    <p>
+      本项目所有权益归开发者以及大创小组成员所有，如有任何疑问课发送邮件至：
+      <el-link href="mailto://xerrors@163.com"><i class="el-icon-message"></i>&nbsp;网易邮箱</el-link>
+      联系我。
+    </p>
   </div>
 </template>
 
 <style lang="stylus" scoped>
-.about
-  padding 0 10rem
-  &__img
-    display block
-    margin 0 auto
-    width 20rem
+.about {
+  padding: 0 10rem;
+  line-height 1.7
+  &__img {
+    display: block;
+    margin: 0 auto;
+    width: 20rem;
+  }
+}
 </style>
