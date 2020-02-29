2cc4ff52c17bb54e44ca253873fad470e3d3f820
Xerrors <xerrors@163.com>
2020-03-01 00:17:36 +0800

    adjust vuex

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,6 @@
 const getters = {
   token: state => state.token,
   avatar: state => state.avatar,
-  name: state => state.name,
-  role: state => state.role,
-  tel: state => state.tel,
   showLogin: state => state.showLogin
 }
 export default getters
--- src/store/index.js
+++ src/store/index.js
@@ -10,10 +10,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    name: '',
-    tel: '',
     token: getToken(), // 因为并没有使用token验证，所以直接用来保存role，仅作用于前端部分
-    avatar: '',
+    avatar: 'http://src.xerrors.fun/blog/20191021/CcFV3DJYgi0B.jpg',
     role: '', // 临时
     showLogin: false
   },
@@ -21,20 +19,8 @@ export default new Vuex.Store({
     SET_TOKEN: (state, token) => {
       state.token = token
     },
-    SET_NAME: (state, name) => {
-      state.name = name
-    },
-    SET_AVATAR: (state, avatar) => {
-      state.avatar = avatar
-    },
     SET_ROLE: (state, role) => {
       state.role = role
-    },
-    SET_DATA: (state, data) => {
-      state.data = data
-    },
-    SET_TEL: (state, tel) => {
-      state.tel = tel
     }
   },
   actions: {
@@ -44,9 +30,6 @@ export default new Vuex.Store({
         login(userForm).then(res => {
           commit('SET_TOKEN', userForm.role)
           commit('SET_ROLE', userForm.role)
-          commit('SET_NAME', res.info.account)
-          commit('SET_TEL', res.info.tel)
-          commit('SET_AVATAR', 'http://src.xerrors.fun/blog/20191021/CcFV3DJYgi0B.jpg')
           setToken(userForm.role)
           localStorage.addresses = JSON.stringify(res.info.addr)
           resolve()
@@ -61,8 +44,6 @@ export default new Vuex.Store({
       commit('SET_ROLE', getToken()) // 使用Cookie保存的身份信息
       return new Promise((resolve, reject) => {
         getInfo().then(res => {
-          commit('SET_NAME', res.info.account)
-          commit('SET_TEL', res.info.tel)
           localStorage.clear()
           localStorage.addresses = JSON.stringify(res.info.addr)
           resolve()
